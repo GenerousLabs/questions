2da1b190115002f4f74184bdc36b32ce25bcffc4
Callum Macdonald <git@callum-macdonald.com>
2020-04-08 16:45:18 +0200

    Move Questions into a scene.

---
+++ gatsby/src/scenes/Questions/Questions.scene.tsx
@@ -0,0 +1,132 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import { Button, Typography } from "@material-ui/core"
+import { LocalDate, ChronoUnit } from "@js-joda/core"
+
+import QuestionIntro from "./components/QuestionIntro.component"
+import QuestionCard from "./components/QuestionCard.component"
+import config from "../../../config"
+
+const { startDayStorageKey } = config
+
+interface IQuestion {
+  slug: string
+  questions: string[]
+}
+
+const getDefaultDayIndex = (): number => {
+  try {
+    const maybe = localStorage.getItem(startDayStorageKey)
+    if (typeof maybe === "string") {
+      const start = LocalDate.parse(maybe)
+      const today = LocalDate.now()
+      return start.until(today, ChronoUnit.DAYS)
+    }
+  } catch (e) {}
+  // TODO Change this back to 0 for deployment
+  // return 0
+  return 1
+}
+
+const saveToday = () => {
+  localStorage.setItem(startDayStorageKey, LocalDate.now().toString())
+  alert("Today has been saved in your browser #Bm1MbR")
+}
+
+const Container = (props: { children: React.ReactNode }) => {
+  const { children } = props
+  return (
+    <div style={{ maxWidth: 760, margin: "0 auto", padding: "20px 0" }}>
+      {children}
+    </div>
+  )
+}
+
+export default (props: IQuestion) => {
+  const { questions } = props
+  const numberOfQuestions = questions.length
+
+  const [hideIntro, setHideIntro] = React.useState(false)
+  const [hideQuestions, setHideQuestions] = React.useState(false)
+  const [dayIndex, setDayIndex] = React.useState(getDefaultDayIndex())
+  const [name, setName] = React.useState("")
+
+  return (
+    <Container>
+      {hideIntro ? (
+        <p style={{ textAlign: "right" }}>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setHideIntro(false)
+            }}
+          >
+            Show intro
+          </Button>
+        </p>
+      ) : (
+        <>
+          <Typography variant="h2" component="h2">
+            Intro
+          </Typography>
+          <QuestionIntro numberOfQuestions={numberOfQuestions} />
+          <p style={{ textAlign: "right" }}>
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={() => {
+                setHideIntro(true)
+              }}
+            >
+              Hide intro
+            </Button>
+          </p>
+        </>
+      )}
+      {hideQuestions ? (
+        <p style={{ textAlign: "right" }}>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setHideQuestions(false)
+            }}
+          >
+            Show questions
+          </Button>
+        </p>
+      ) : (
+        <>
+          <Typography variant="h2" component="h2">
+            Questions
+          </Typography>
+          <ol>
+            {questions.map((question, i) => (
+              <li key={i}>{question}</li>
+            ))}
+          </ol>
+          <p style={{ textAlign: "right" }}>
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={() => {
+                setHideQuestions(true)
+              }}
+            >
+              Hide questions
+            </Button>
+          </p>
+        </>
+      )}
+      <Typography variant="h2">Today's questions</Typography>
+      <QuestionCard
+        name={"John"}
+        questions={questions}
+        dayIndex={dayIndex}
+        setDayIndex={setDayIndex}
+        numberOfQuestions={numberOfQuestions}
+      />
+      <Typography>Start another question sequence</Typography>
+      <Button variant="outlined">Click to start</Button>
+    </Container>
+  )
+}
--- gatsby/src/templates/question.tsx
+++ gatsby/src/templates/question.tsx
@@ -1,13 +1,7 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-import { Button, Typography } from "@material-ui/core"
-import { LocalDate, ChronoUnit } from "@js-joda/core"
 
-import QuestionIntro from "../components/shared/QuestionIntro.component"
-import QuestionCard from "../components/shared/QuestionCard.component"
-import config from "../config"
-
-const { startDayStorageKey } = config
+import QuestionsScene from "../scenes/Questions/Questions.scene"
 
 interface IQuestion {
   data: {
@@ -18,121 +12,9 @@ interface IQuestion {
   }
 }
 
-const getDefaultDayIndex = (): number => {
-  try {
-    const maybe = localStorage.getItem(startDayStorageKey)
-    if (typeof maybe === "string") {
-      const start = LocalDate.parse(maybe)
-      const today = LocalDate.now()
-      return start.until(today, ChronoUnit.DAYS)
-    }
-  } catch (e) {}
-  // TODO Change this back to 0 for deployment
-  // return 0
-  return 1
-}
-
-const saveToday = () => {
-  localStorage.setItem(startDayStorageKey, LocalDate.now().toString())
-  alert("Today has been saved in your browser #Bm1MbR")
-}
-
-const Container = (props: { children: React.ReactNode }) => {
-  const { children } = props
-  return (
-    <div style={{ maxWidth: 760, margin: "0 auto", padding: "20px 0" }}>
-      {children}
-    </div>
-  )
-}
-
 export default ({ data }: IQuestion) => {
-  const { questions } = data.questionsYaml
-  const numberOfQuestions = questions.length
-
-  const [hideIntro, setHideIntro] = React.useState(false)
-  const [hideQuestions, setHideQuestions] = React.useState(false)
-  const [dayIndex, setDayIndex] = React.useState(getDefaultDayIndex())
-  const [name, setName] = React.useState("")
-
-  return (
-    <Container>
-      {hideIntro ? (
-        <p style={{ textAlign: "right" }}>
-          <Button
-            variant="outlined"
-            onClick={() => {
-              setHideIntro(false)
-            }}
-          >
-            Show intro
-          </Button>
-        </p>
-      ) : (
-        <>
-          <Typography variant="h2" component="h2">
-            Intro
-          </Typography>
-          <QuestionIntro numberOfQuestions={numberOfQuestions} />
-          <p style={{ textAlign: "right" }}>
-            <Button
-              variant="outlined"
-              size="small"
-              onClick={() => {
-                setHideIntro(true)
-              }}
-            >
-              Hide intro
-            </Button>
-          </p>
-        </>
-      )}
-      {hideQuestions ? (
-        <p style={{ textAlign: "right" }}>
-          <Button
-            variant="outlined"
-            onClick={() => {
-              setHideQuestions(false)
-            }}
-          >
-            Show questions
-          </Button>
-        </p>
-      ) : (
-        <>
-          <Typography variant="h2" component="h2">
-            Questions
-          </Typography>
-          <ol>
-            {questions.map((question, i) => (
-              <li key={i}>{question}</li>
-            ))}
-          </ol>
-          <p style={{ textAlign: "right" }}>
-            <Button
-              variant="outlined"
-              size="small"
-              onClick={() => {
-                setHideQuestions(true)
-              }}
-            >
-              Hide questions
-            </Button>
-          </p>
-        </>
-      )}
-      <Typography variant="h2">Today's questions</Typography>
-      <QuestionCard
-        name={"John"}
-        questions={questions}
-        dayIndex={dayIndex}
-        setDayIndex={setDayIndex}
-        numberOfQuestions={numberOfQuestions}
-      />
-      <Typography>Start another question sequence</Typography>
-      <Button variant="outlined">Click to start</Button>
-    </Container>
-  )
+  const { slug, questions } = data.questionsYaml
+  return <QuestionsScene slug={slug} questions={questions} />
 }
 
 export const query = graphql`
