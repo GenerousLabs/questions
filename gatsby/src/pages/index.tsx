a266a969ac10056ac099180729b2a3c143994ec4
Callum Macdonald <git@callum-macdonald.com>
2020-04-14 17:02:37 +0200

    Add a basic index page.

---
+++ gatsby/src/pages/index.tsx
@@ -0,0 +1,31 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { makeStyles, Typography } from "@material-ui/core"
+
+const Index = () => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h1">Work in progress</Typography>
+      <p>Try some of these questions to get started</p>
+      <p>
+        <Link to="/sean">Sean</Link>
+      </p>{" "}
+      <p>
+        <Link to="/relationships">Relationships</Link>
+      </p>
+    </div>
+  )
+}
+
+export default Index
+
+const useStyles = makeStyles((theme) => {
+  return {
+    container: {
+      textAlign: "center",
+      padding: 40,
+    },
+  }
+})
