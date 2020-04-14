68ae28f05cfd24bc66999a2b86cd9a660641a42c
Callum Macdonald <git@callum-macdonald.com>
2020-04-14 17:03:10 +0200

    Add SEO to homepage.

--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -2,11 +2,14 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { makeStyles, Typography } from "@material-ui/core"
 
+import SEO from "../scenes/SEO/SEO.scene"
+
 const Index = () => {
   const classes = useStyles()
 
   return (
     <div className={classes.container}>
+      <SEO title="Question Challenge" />
       <Typography variant="h1">Work in progress</Typography>
       <p>Try some of these questions to get started</p>
       <p>
