c00b2d7f50fe30c7b981db1185d5ad7e641f1f05
Callum Macdonald <git@callum-macdonald.com>
2020-04-14 16:21:14 +0200

    Update get started to mostly match design.

--- gatsby/src/scenes/Questions/components/GetStarted.component.tsx
+++ gatsby/src/scenes/Questions/components/GetStarted.component.tsx
@@ -1,12 +1,66 @@
 import * as React from "react"
-import { Typography } from "@material-ui/core"
+import { Grid, makeStyles, Typography } from "@material-ui/core"
 
 const GetStarted = () => {
+  const classes = useStyles()
+
   return (
-    <Typography style={{ padding: 100, textAlign: "center" }}>
-      You haven't started yet. Click start to get something going.
-    </Typography>
+    <Grid container className={classes.container}>
+      <Grid item xs={4} className={classes.child}>
+        <Typography className={classes.number}>1</Typography>
+        <Typography className={classes.title}>Invite a friend</Typography>
+        <Typography className={classes.p}>
+          Find someone you'd like to know better, send them this page, invite
+          them to play.
+        </Typography>
+      </Grid>
+      <Grid item xs={4} className={classes.child}>
+        <Typography className={classes.number}>2</Typography>
+        <Typography className={classes.title}>Make the commitment</Typography>
+        <Typography className={classes.p}>
+          Swear a blood oath (or don't) that you will commit at least 10 minutes
+          a day for 28 days.
+        </Typography>
+      </Grid>
+      <Grid item xs={4} className={classes.child}>
+        <Typography className={classes.number}>3</Typography>
+        <Typography className={classes.title}>Get started together</Typography>
+        <Typography className={classes.p}>
+          Make sure your question sets are synchronised by clicking 'add a
+          person' at the same time.
+        </Typography>
+      </Grid>
+    </Grid>
   )
 }
 
 export default GetStarted
+
+const useStyles = makeStyles((theme) => {
+  return {
+    container: {
+      backgroundColor: "#e3e6eb",
+    },
+    child: {
+      textAlign: "center",
+      padding: 36,
+    },
+    number: {
+      color: "#fff",
+      backgroundColor: "#000",
+      borderRadius: "50%",
+      width: 20,
+      height: 20,
+      margin: "0 auto",
+      fontSize: 14,
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: "bold",
+      marginTop: 12,
+    },
+    p: {
+      fontSize: 16,
+    },
+  }
+})
