12e2ff45f37276ae2769619ddf8e4d43f36dba33
Callum Macdonald <git@callum-macdonald.com>
2020-04-15 11:01:04 +0200

    Remove unused cruft.

--- gatsby/src/config.ts
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  countOfInitiallyShownPosts: 5,
-  countOfPostsDynamicallyAdded: 5,
-  offsetHeightToTriggerLoading: 20,
-  startDayStorageKey: "__startDate",
-}
-
-export default CONFIG
