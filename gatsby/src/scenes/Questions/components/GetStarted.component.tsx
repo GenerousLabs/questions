b935540e3f71f3b4ac71cb82a8ea2dd51daacedc
Sean Coley <seanwcoley@gmail.com>
2020-04-17 15:05:03 +0200

    Add title and minor fixes to GetStarted component

--- gatsby/src/scenes/Questions/components/GetStarted.component.tsx
+++ gatsby/src/scenes/Questions/components/GetStarted.component.tsx
@@ -12,28 +12,33 @@ const GetStarted = (props: Props) => {
   return (
     <div>
       <Grid container className={classes.container}>
-        <Grid item xs={12} >
-          <Typography className={classes.mainTitle}>How to play</Typography>
+        <Grid item xs={12}>
+          <Typography className={classes.mainTitle}>
+            Quick start guide
+          </Typography>
         </Grid>
         <Grid item sm={4} xs={12} className={classes.child}>
           <Typography className={classes.number}>1</Typography>
           <Typography className={classes.title}>Start together</Typography>
           <Typography className={classes.p}>
-            Pick someone you want to know better. Send them this link. Click 'Add a Person' together on the same day.
+            Pick someone you want to know better. Send them this link. Click
+            'Add a Person' together on the same day.
           </Typography>
         </Grid>
         <Grid item sm={4} xs={12} className={classes.child}>
           <Typography className={classes.number}>2</Typography>
           <Typography className={classes.title}>Share your answer</Typography>
           <Typography className={classes.p}>
-            Each day, share your answer with your question partner. Discuss over the phone, by text, or by carrier pigeon
+            Each day, share your answer with your question partner. Discuss over
+            the phone, by text, or by carrier pigeon
           </Typography>
         </Grid>
         <Grid item sm={4} xs={12} className={classes.child}>
           <Typography className={classes.number}>3</Typography>
           <Typography className={classes.title}>Rinse and repeat</Typography>
           <Typography className={classes.p}>
-            New questions will appear here each day. Come back to see what it is and share your next answer with your partner.
+            New questions will appear here each day. Come back to see what it is
+            and share your next answer with your partner.
           </Typography>
         </Grid>
       </Grid>
@@ -68,7 +73,7 @@ const useStyles = makeStyles((theme) => {
       marginTop: 12,
       marginBottom: -12,
       paddingTop: 12,
-      textAlign: "center"
+      textAlign: "center",
     },
     title: {
       fontSize: 16,
